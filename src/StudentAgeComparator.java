425c05cab47dc9bf1fc4eb6d0df7972cc1676872
poojayad <poojayadavtech@gmail.com>
2022-08-21 01:26:46 +0530

    few details added wrt Interger class

--- src/StudentAgeComparator.java
+++ src/StudentAgeComparator.java
@@ -7,13 +7,19 @@ public class StudentAgeComparator implements Comparator<Student> {
 
         return Integer.compare(student1.age, student2.age);
 
-            //          either of the lines can be used.
-//        if(student1.age == student2.age)
-//            return 0;
-//        else if (student1.age>student2.age) {
-//            return 1;
-//        }else {
-//            return -1;
-//        }
+        // this will also work but only if data type of age provide in class Integer not primitive int,
+        // compare method is local to class Integer. Whereas compareTo is provided by the Comparator.
+        /* return student1.age.compareTo(student2.age); */
+
+        // either of the lines can be used.
+        /*
+        if(student1.age == student2.age)
+            return 0;
+        else if (student1.age>student2.age) {
+            return 1;
+        }else {
+            return -1;
+        }
+        */
     }
 }
